c91ad34237345c9bc90bf8c9c46bfb6c502c0ed9
Raymon <luotyzl@gmail.com>
2018-02-13 13:19:54 +1300

    added firefox tests

--- C#/PaymentPageTest/PaymentPageTest.cs
+++ C#/PaymentPageTest/PaymentPageTest.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
 using Selenium;
@@ -12,86 +13,107 @@ namespace PaymentPageTest
     [TestClass]
     public class PaymentPageTest
     {
-        private readonly IWebDriver _driver = new ChromeDriver();
+        private readonly IWebDriver _chromeDriver = new ChromeDriver();
+        private readonly IWebDriver _fireFoxDriver = new FirefoxDriver();
         private const string Weburl = "http://stagingmerchant.myrapidpay.com/trade/payment.action?token=79f077de5d09ce3244301656a8ac0d00e69d3d7d05cc5bb2";
 
         [TestMethod]
-        public void PerfermanceTest()
+        public void PerfermanceOnChrome()
         {
-            _driver.Navigate().GoToUrl(Weburl);
+            var driver = PerfermanceTest(_chromeDriver);
+            Assert.IsTrue(driver.Title.Contains("中国建设银行"));
+            driver.Quit();
+        }
 
+        [TestMethod]
+        public void PerfermanceOnFirefox()
+        {
+            var driver = PerfermanceTest(_fireFoxDriver);
+            Assert.IsTrue(driver.Title.Contains("中国建设银行"));
+            driver.Quit();
+        }
+        public IWebDriver PerfermanceTest(IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl(Weburl);
+
+            //check language is chinese
+            var languageTag = driver.FindElement(By.ClassName("lan-container"));
+            var languateSpan = languageTag.FindElement(By.TagName("span"));
+            if (languateSpan.Text == "中文")
+            {
+                languageTag.Click();
+            }
             //Input right information
-            var cardName = _driver.FindElement(By.Id("idCardName"));
+            var cardName = driver.FindElement(By.Id("idCardName"));
             cardName.Clear();
             cardName.SendKeys("姚鑫");
-            var card = _driver.FindElement(By.Name("idCard"));
+            var card = driver.FindElement(By.Name("idCard"));
             card.Clear();
             card.SendKeys("13010519811103125X");
-            var email = _driver.FindElement(By.Name("email"));
+            var email = driver.FindElement(By.Name("email"));
             email.Clear();
             email.SendKeys("test@gmail.com");
-            var telNo = _driver.FindElement(By.Name("telNo"));
+            var telNo = driver.FindElement(By.Name("telNo"));
             telNo.Clear();
             telNo.SendKeys("64");
-            var telNum = _driver.FindElement(By.Name("telNum"));
+            var telNum = driver.FindElement(By.Name("telNum"));
             telNum.Clear();
             telNum.SendKeys("123456");
-            var amount = _driver.FindElement(By.Name("amount"));
+            var amount = driver.FindElement(By.Name("amount"));
             amount.Clear();
             amount.SendKeys("99");
-            var reference = _driver.FindElement(By.Name("reference"));
+            var reference = driver.FindElement(By.Name("reference"));
             reference.Clear();
             reference.SendKeys("test reference");
 
-            var submitBtn = _driver.FindElement(By.Id("goPayId"));
+            var submitBtn = driver.FindElement(By.Id("goPayId"));
             submitBtn.Click();
 
             //Wait page loaded
-            WebDriverWait submitWait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            WebDriverWait submitWait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             submitWait.Until(ExpectedConditions.ElementExists(By.Id("pay_now")));
-            var payBtn = _driver.FindElement(By.Id("pay_now"));
-            var ccb = _driver.FindElement(By.ClassName("bank02"));
+            var payBtn = driver.FindElement(By.Id("pay_now"));
+            var ccb = driver.FindElement(By.ClassName("bank02"));
             ccb.Click();
             payBtn.Click();
             Thread.Sleep(20000);
 
             //Wait openning bank payment page in new  tab
-            var windowHandle = _driver.WindowHandles;
+            var windowHandle = driver.WindowHandles;
             foreach (var window in windowHandle)
             {
-                _driver.SwitchTo().Window(window);
-                if (_driver.Title != "Payment") break; 
+                driver.SwitchTo().Window(window);
+                if (driver.Title != "Payment") break; 
             }
             //Check if open the bank page
-            Assert.IsTrue(_driver.Title.Contains("中国建设银行"));
-            _driver.Quit();
+            return driver;
         }
 
         [TestMethod]
         public void EmptyInputTest()
         {
-            _driver.Navigate().GoToUrl(Weburl);
+            _chromeDriver.Navigate().GoToUrl(Weburl);
 
             //Input empty information
-            var cardName = _driver.FindElement(By.Id("idCardName"));
+            var cardName = _chromeDriver.FindElement(By.Id("idCardName"));
             cardName.Clear();
-            var card = _driver.FindElement(By.Name("idCard"));
+            var card = _chromeDriver.FindElement(By.Name("idCard"));
             card.Clear();
-            var email = _driver.FindElement(By.Name("email"));
+            var email = _chromeDriver.FindElement(By.Name("email"));
             email.Clear();
-            var telNo = _driver.FindElement(By.Name("telNo"));
+            var telNo = _chromeDriver.FindElement(By.Name("telNo"));
             telNo.Clear();
-            var telNum = _driver.FindElement(By.Name("telNum"));
+            var telNum = _chromeDriver.FindElement(By.Name("telNum"));
             telNum.Clear();
-            var amount = _driver.FindElement(By.Name("amount"));
+            var amount = _chromeDriver.FindElement(By.Name("amount"));
             amount.Clear();
-            var reference = _driver.FindElement(By.Name("reference"));
+            var reference = _chromeDriver.FindElement(By.Name("reference"));
             reference.Clear();
 
             //Click next button
-            var submitBtn = _driver.FindElement(By.Id("goPayId"));
+            var submitBtn = _chromeDriver.FindElement(By.Id("goPayId"));
             submitBtn.Click();
-            var errorMessages = _driver.FindElements(By.ClassName("formError"));
+            var errorMessages = _chromeDriver.FindElements(By.ClassName("formError"));
             var wrongMessageFlg = false;
 
             //check all validation message
@@ -104,44 +126,44 @@ namespace PaymentPageTest
             }
             //Check if open the bank page
             Assert.IsFalse(wrongMessageFlg);
-            _driver.Quit();
+            _chromeDriver.Quit();
         }
 
         [TestMethod]
         public void NotSeletBankTest()
         {
-            _driver.Navigate().GoToUrl(Weburl);
+            _chromeDriver.Navigate().GoToUrl(Weburl);
 
             //Input right information
-            var cardName = _driver.FindElement(By.Id("idCardName"));
+            var cardName = _chromeDriver.FindElement(By.Id("idCardName"));
             cardName.Clear();
             cardName.SendKeys("姚鑫");
-            var card = _driver.FindElement(By.Name("idCard"));
+            var card = _chromeDriver.FindElement(By.Name("idCard"));
             card.Clear();
             card.SendKeys("13010519811103125X");
-            var email = _driver.FindElement(By.Name("email"));
+            var email = _chromeDriver.FindElement(By.Name("email"));
             email.Clear();
             email.SendKeys("test@gmail.com");
-            var amount = _driver.FindElement(By.Name("amount"));
+            var amount = _chromeDriver.FindElement(By.Name("amount"));
             amount.Clear();
             amount.SendKeys("99");
-            var reference = _driver.FindElement(By.Name("reference"));
+            var reference = _chromeDriver.FindElement(By.Name("reference"));
             reference.Clear();
             reference.SendKeys("test reference");
 
             //Click next button
-            var submitBtn = _driver.FindElement(By.Id("goPayId"));
+            var submitBtn = _chromeDriver.FindElement(By.Id("goPayId"));
             submitBtn.Click();
 
             //Wait page loaded
-            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            WebDriverWait wait = new WebDriverWait(_chromeDriver, TimeSpan.FromSeconds(10));
             wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.Id("pay_now")));
-            var payBtn = _driver.FindElement(By.Id("pay_now"));
+            var payBtn = _chromeDriver.FindElement(By.Id("pay_now"));
             payBtn.Click();
 
-            var bankError = _driver.FindElement(By.Id("bank_err"));
+            var bankError = _chromeDriver.FindElement(By.Id("bank_err"));
             Assert.IsTrue(bankError.Text.Contains("请选择银行"));
-            _driver.Quit();
+            _chromeDriver.Quit();
         }
     }
 }
--- Java/src/Test/PaymentTest.java
+++ Java/src/Test/PaymentTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -14,24 +15,81 @@ import junit.framework.TestCase;
 
 public class PaymentTest extends TestCase{
 	
-	private WebDriver setUpDriver() {
-		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-        //Launch the Chrome and open Website
-		driver.get("http://stagingmerchant.myrapidpay.com/trade/payment.action?token=79f077de5d09ce3244301656a8ac0d00e69d3d7d05cc5bb2");
-		return driver;
+	private enum browser{
+		Chrome,
+		Firefox
 	}
+	
+	private WebDriver setUpDriver(browser browser) {
+		
+		if(browser == browser.Chrome) {
+			System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+			WebDriver chromeDriver = new ChromeDriver();
+	        //Launch the Chrome and open Website
+			chromeDriver.get("http://stagingmerchant.myrapidpay.com/trade/payment.action?token=79f077de5d09ce3244301656a8ac0d00e69d3d7d05cc5bb2");
+			return chromeDriver;
+		} else {
+			String service = "driver/geckodriver.exe";  
+			System.setProperty("webdriver.gecko.driver", service);
+			WebDriver fireFoxDriver = new FirefoxDriver();
+	        //Launch the firefox and open Website
+			fireFoxDriver.get("http://stagingmerchant.myrapidpay.com/trade/payment.action?token=79f077de5d09ce3244301656a8ac0d00e69d3d7d05cc5bb2");
+			return fireFoxDriver;
+		}
+		
+	}
+	
+	//test on chrome browser
 	public void testPerfermanceOnChrome() throws InterruptedException {
-		;
-		WebDriver driver = perfermanceTest(setUpDriver());
+		WebDriver driver = PerfermanceTest(setUpDriver(browser.Chrome));
 		assertTrue(driver.getTitle().contains("中国建设银行"));
-		System.out.println("test perfermance successfully");
+		System.out.println("test perfermance on chrome successfully");
 		driver.quit();
 	}
 	
+	//test on firefox browser
+	public void testPerfermanceOnFirefox() throws InterruptedException {
+		WebDriver driver = PerfermanceTest(setUpDriver(browser.Firefox));
+		assertTrue(driver.getTitle().contains("中国建设银行"));
+		System.out.println("test perfermance on firefox successfully");
+		driver.quit();
+	}
+	
+	public void testEmptyInputOnChrome() throws InterruptedException {
+		WebDriver driver = setUpDriver(browser.Chrome);
+		boolean testResult = EmptyInput(driver);
+		assertFalse(testResult);
+		System.out.println("test empty input on chrome successfully");
+		driver.quit();
+	}
+	
+	public void testEmptyInputOnFirefox() throws InterruptedException {
+		WebDriver driver = setUpDriver(browser.Firefox);
+		boolean testResult = EmptyInput(driver);
+		assertFalse(testResult);
+		System.out.println("test empty input on firefox successfully");
+		driver.quit();
+	}
+
+	public void testNotSelectBankOnChrome() throws InterruptedException {
+		WebDriver driver = setUpDriver(browser.Chrome);
+		String testResult = NotSelectBank(driver);
+        assertTrue(testResult.contains("请选择银行") || testResult.contains("Select your bank"));
+        System.out.println("test not select bank on firefox successfully");
+        driver.quit();
+	}
+	
+	public void testNotSelectBankOnFirefox() throws InterruptedException {
+		WebDriver driver = setUpDriver(browser.Firefox);
+		String testResult = NotSelectBank(driver);
+        assertTrue(testResult.contains("请选择银行") || testResult.contains("Select your bank"));
+        System.out.println("test not select bank on firefox successfully");
+        driver.quit();
+	}
+	
 	//Input all right information and select ccb bank 
-	public WebDriver perfermanceTest(WebDriver driver) throws InterruptedException{		
- 
+	public WebDriver PerfermanceTest(WebDriver driver) throws InterruptedException{		
+		
 		//Input right information
 		WebElement cardName = driver.findElement(By.id("idCardName"));
         cardName.clear();
@@ -80,9 +138,8 @@ public class PaymentTest extends TestCase{
 	}
 	
 	//Test empty input and click next
-	public void testEmptyInput() throws InterruptedException{
-		WebDriver driver = setUpDriver();
- 
+	public boolean EmptyInput(WebDriver driver) throws InterruptedException{
+
 		//Input Empty information
 		WebElement cardName = driver.findElement(By.id("idCardName"));
         cardName.clear();
@@ -103,21 +160,17 @@ public class PaymentTest extends TestCase{
         List<WebElement> errorMessages = driver.findElements(By.className("formError"));
         boolean wrongMessageFlg = false;
         for(WebElement message : errorMessages) {
-        	if(!message.getText().contains("此处不可空白")) {
+        	if(!message.getText().contains("此处不可空白") && !message.getText().contains("This field cannot be left blank")) {
         		wrongMessageFlg = true;
         		System.out.println(message.getText());
         	}
         }
-        assertFalse(wrongMessageFlg);
-        System.out.println("test empty input successfully");
-        driver.quit();
+        return wrongMessageFlg;
 	}
 	
 	//Test if use don't select bank
-	public void testNotSelectBank() {
+	public String NotSelectBank(WebDriver driver) { 
 		
-		WebDriver driver = setUpDriver();
- 
 		//Input right information
 		WebElement cardName = driver.findElement(By.id("idCardName"));
         cardName.clear();
@@ -146,8 +199,7 @@ public class PaymentTest extends TestCase{
         payBtn.click();
         
         WebElement bankError = driver.findElement(By.id("bank_err"));
-        assertTrue(bankError.getText().contains("请选择银行"));
-        System.out.println("test not select bank successfully");
-        driver.quit();
+        return bankError.getText();
 	}
+	
 }
